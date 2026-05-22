b62dae449c16a443536c2157a3c66ca8cf1b9963
arthur antonelli <arthur.antonelli76@gmail.com>
2026-05-22 15:21:13 -0300

    feat: criar script para adicionar o resto dos modelos 3D no projeto e instalar o unziper

---
+++ projeto/scripts/download-models.js
@@ -0,0 +1,19 @@
+import { existsSync, mkdirSync, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { Extract } from 'unzipper';
+
+const MODELS_URL = 'https://github.com/seu-usuario/seu-repo/releases/download/v1.0-models/models.zip';
+const DEST = './public/models';
+
+if (existsSync(DEST)) {
+  console.log('Modelos já existem, pulando download.');
+  process.exit(0);
+}
+
+console.log('Baixando modelos...');
+mkdirSync(DEST, { recursive: true });
+
+const res = await fetch(MODELS_URL);
+await pipeline(res.body, Extract({ path: './public' }));
+
+console.log('Modelos extraídos em public/models');
